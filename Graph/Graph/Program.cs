ee5a44cce2b9fd93a4814ad8deee4ec7a47fa298
LunA <55641527+LunA820@users.noreply.github.com>
2021-07-31 01:20:19 +1000

    Improved API

--- Graph/Graph/Program.cs
+++ Graph/Graph/Program.cs
@@ -8,32 +8,43 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
-            Node n8 = new Node(8);
-            Node n9 = new Node(9);
-            Node n10 = new Node(10);
-            Node n11 = new Node(11);
+            // Add node 1-11 to Graph
+            GraphAPI G = new GraphAPI();
+            Node n1 = G.addNode(1); Node n2 = G.addNode(2); Node n3 = G.addNode(3);
+            Node n4 = G.addNode(4); Node n5 = G.addNode(5); Node n6 = G.addNode(6);
+            Node n7 = G.addNode(7); Node n8 = G.addNode(8); Node n9 = G.addNode(9);
+            Node n10 = G.addNode(10); Node n11 = G.addNode(11);
 
-            n1.addAdjNode(n2); n1.addAdjNode(n4); n1.addAdjNode(n5);
-            n2.addAdjNode(n1); n2.addAdjNode(n5); n2.addAdjNode(n3);
-            n3.addAdjNode(n2); n3.addAdjNode(n8); n3.addAdjNode(n9);
-            n4.addAdjNode(n1); n4.addAdjNode(n6); n4.addAdjNode(n11);
-            n5.addAdjNode(n2); n5.addAdjNode(n1);
-            n6.addAdjNode(n4); n6.addAdjNode(n7);
-            n7.addAdjNode(n6); n7.addAdjNode(n9);
-            n8.addAdjNode(n3);
-            n9.addAdjNode(n3); n9.addAdjNode(n7); n9.addAdjNode(n10);
-            n10.addAdjNode(n9);
-            n11.addAdjNode(n4);
+            // Connect Graph
+            G.link(1, 2); G.link(1, 5); G.link(1, 4);
+            G.link(2, 5); G.link(2, 3);
+            G.link(3, 8); G.link(3, 9);
+            G.link(9, 10); G.link(9, 7);
+            G.link(6, 7); G.link(6, 4);
+            G.link(4, 11);
 
-            ArrayList T = BFS.traversal(n1);
-            GraphAPI.printPath(T);
+            // Path should be n4 -> n11 (Adj. nodes)
+            ArrayList p0 = G.find_dfs_path(4, 11);
+            Console.Write("n4 to n11: ");
+            foreach (Node n in p0)
+                Console.Write(n.getX() + " ");
+
+
+
+            // Path should be n1 -> n2 -> n3 -> n8
+            ArrayList p1 = G.find_dfs_path(1, 8);
+            Console.Write("n1 to n8: ");
+            foreach (Node n in p1)
+                Console.Write(n.getX() + " ");
+
+
+            // Path should be n10 -> n9 -> n7 -> n6
+            ArrayList p2 = G.find_dfs_path(10, 6);
+            Console.Write("n10 to n16: ");
+            foreach (Node n in p2)
+                Console.Write(n.getX() + " ");
+
+
 
         }
     }
